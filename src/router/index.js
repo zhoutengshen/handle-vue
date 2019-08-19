48095a6ed0326af31b9206bb2b8b2d6e951f3188
zhoutengshen <1724847624@qq.com>
2019-08-19 23:51:22 +0800

    ok

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import VueRouter from "vue-router";
+/**
+ * @type {import("vue-router").RouterOptions}
+ */
+const routerOptions = {
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            redirect: "/home"
+        },
+        {
+            path: "/home",
+            name: "name",
+            component: () => import("@/pages/home")
+        }
+    ]
+}
+
+export default new VueRouter({
+    ...routerOptions
+})
